034abb1c350e3a441cc8141efb30ab71d1d64265
Kane <kane@kane.ventures>
2018-02-14 11:51:03 -0700

    2

--- mnwatcher.bash
+++ mnwatcher.bash
@@ -2,13 +2,14 @@
 
 COINCLI=$1
 OUTPUTS=$2
-
+NAME=`hostname`
 
 $COINCLI masternodelist | grep $OUTPUTS
 
 
-if $COINCLI masternodelist | grep $OUTPUTS; then
+if $COINCLI masternodelist | grep -w $OUTPUTS; then
   echo exists
 else
   echo not found
+  ./nma.sh MNWatcher MNList "I don't see $NAME on the list" 2 &
 fi
