aa62579b0e24ae54f16428760dee0d9c5dd6190a
Kane <kane@kane.ventures>
2018-02-16 01:18:43 -0700

    1

--- mnwatcher.bash
+++ mnwatcher.bash
@@ -8,5 +8,5 @@ if $COINCLI masternodelist | grep -w $OUTPUTS | grep -q ENABLED; then
   echo "$NAME exists on MNList"
 else
   echo "$NAME does not exist on MNList"
-  ./nma.sh MNWatcher MNList "I don't see $NAME on the list" 2 &
+  /home/mentatmind/mnbootstrap/nma.sh MNWatcher MNList "I don't see $NAME on the list" 0 &
 fi
--- serversetup.bash
+++ serversetup.bash
@@ -1,16 +1,31 @@
 #!/bin/bash
 
 #### Add Swap
+if [ ! -f /mnt/2GB.swap ]; then
 echo "Adding Swap"
 fallocate -l 2G /mnt/2GB.swap
 mkswap /mnt/2GB.swap
 swapon /mnt/2GB.swap
 echo "/mnt/2GB.swap  none  swap  sw 0  0" >> /etc/fstab
 echo "vm.swappiness=80" >> /etc/sysctl.conf
+fi
 
 #### Set SSH
 echo "Setting SSH"
 sed -i '/Port 22/c\Port 31680' /etc/ssh/sshd_config
+
+cd /home/mentatmind
+git clone https://github.com/kanewinter/paybot.git
+mkdir /home/mentatmind/go
+if [ -z $GOPATH ]; then
+echo "export GOPATH=/home/mentatmind/go" >> $HOME/.bashrc
+fi
+. $HOME/.bashrc
+touch customerdata.dat
+touch payconfig.toml
+go get github.com/spf13/viper
+
+
 
 #### Updates
 apt-get update
@@ -19,6 +34,3 @@ apt-get install -y build-essential libtool autotools-dev autoconf pkg-config lib
 add-apt-repository -y ppa:bitcoin/bitcoin
 apt-get update
 apt-get install -y libdb4.8-dev libdb4.8++-dev libboost-all-dev libevent-dev libminiupnpc-dev
-
-cd /root/
-git clone https://github.com/kanewinter/paybot.git
